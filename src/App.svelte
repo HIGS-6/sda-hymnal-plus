794310baef72688e3a2cb667fddeefb372ebfa95
Hector Isaac Game Suarez <hectorisaacg.s@hotmail.com>
2024-03-27 17:58:14 +0000

    Cleaner logic in separate ts files, implemented slide system (Hymns have covers showing the title only, hymns with refrains will be displayed properly), ready for next step (Bi-lingual display)

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,13 @@
 <script lang="ts">
-  import PocketBase from "pocketbase";
-
-  class Hymn {
-    title: string;
-    number: number;
-    lyrics: Map<String, String>;
-    constructor(title: string, number: number, lyrics: Map<String, String>) {
-      this.title = title;
-      this.number = number;
-      this.lyrics = lyrics;
-    }
-  }
-  const pb = new PocketBase("https://sda-api.pockethost.io/");
+  import { Hymn, Slide } from "./lib/models";
+  import { getHymn } from "./lib/sda_api.ts"; 
 
   let currentHymn: Hymn | undefined;
-  let inputHymnNumber = 1;
   let currentStanzaIdx = 0;
   let currentHymnNumber = "";
 
-  async function getHymn() {
-    currentStanzaIdx = 0;
-    try {
-      // Get Hymn
-      const hymn_result = await pb
-        .collection("hymns")
-        .getFirstListItem(
-          `number=${inputHymnNumber} && language.langName="English"`,
-        );
-
-      // Get Lyrics
-      const lyricsResult = await pb.collection("hymn_lyrics").getFullList({
-        filter: `hymn="${hymn_result.id}"`,
-      });
-
-      // Instantiate an Hymn object
-      let title = hymn_result.title;
-      let number = hymn_result.number;
-      let lyrics = new Map();
-
-      lyricsResult.forEach((rawLyric) => {
-        // Adds a Map for each Stanza
-        lyrics.set(rawLyric.context, rawLyric.content);
-      });
-
-      let hymn = new Hymn(title, number, lyrics);
-
-      // return hymn;
-      currentHymn = hymn;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  function getStanzaInfo(number: number, hymn: Hymn): Array<String> {
-    let lyricValues = Array.from(hymn.lyrics.values());
-    let lyricKeys = Array.from(hymn.lyrics.keys());
-
-    if (number < 0 || number > hymn.lyrics.size) {
-      console.log("Wrong Index");
-    } else {
-      return [lyricKeys[number], lyricValues[number]];
-    }
-
-    return ["Error", "Error"];
-  }
+  let currentSlides: Array<Slide> | undefined;
+  let currentSlideIndex: number = 0;
 
   document.addEventListener(
     "keydown",
@@ -81,24 +25,25 @@
       }
       // Enter pressed so, the current number is used to load the hymn
       else if (codeValue === "Enter") {
-        inputHymnNumber = parseInt(currentHymnNumber);
-        await getHymn();
+        let currentHymn = await getHymn(parseInt(currentHymnNumber));
         currentHymnNumber = "";
+        
+        if (currentHymn instanceof Hymn) {
+          currentSlides = currentHymn.getSlides();
+        } // Aqui un else y hacer algo con el mensaje de error. 
       }
-
-      console.log(`Current Hymn Number: ${currentHymnNumber}`);
-
-      if (!currentHymn) return;
+      
+      if (!currentSlides) return;
       // Left arrow pressed, so previous stanza is shown
       if (codeValue === "ArrowLeft") {
-        if (currentStanzaIdx > 0) {
-          currentStanzaIdx--;
+        if (currentSlideIndex > 0) {
+          currentSlideIndex--;
         }
       }
       // Right arrow pressed, so next stanza is shown
       else if (codeValue === "ArrowRight") {
-        if (currentStanzaIdx + 1 < currentHymn.lyrics.size) {
-          currentStanzaIdx++;
+        if (currentSlideIndex + 1 < currentSlides.length) {
+          currentSlideIndex++;
         }
       }
     },
@@ -107,25 +52,23 @@
 </script>
 
 <main class="container">
-  <!-- <div class="center"> -->
-  {#if currentHymn instanceof Hymn}
+  {#if currentSlides}
     <ul class="center">
-      {#each getStanzaInfo(currentStanzaIdx, currentHymn)[1].split("\n") as line, _}
+      {#each currentSlides[currentSlideIndex].primaryString.split("\n") as line, _}
         <p class="stanza">{line}</p>
       {/each}
     </ul>
     <h3 style="text-align: center;">
-      {getStanzaInfo(currentStanzaIdx, currentHymn)[0]}
+      {currentSlides[currentSlideIndex].secondaryString}
     </h3>
   {:else}
-    <h1 style="font-size: 100px;">SDA Hymnal+</h1>
+    <h1 style="font-size: 80px;">SDA Hymnal+</h1>
   {/if}
-  <!-- </div> -->
 </main>
 
 <style>
   .stanza {
-    font-size: 75px;
+    font-size: 70px;
     font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
     text-align: left;
     list-style: none;
---
+++ src/lib/models.ts
@@ -0,0 +1,54 @@
+export class Slide {
+  primaryString: string;
+  secondaryString: string;
+  constructor(primary: string, secondary: string) {
+    this.primaryString = primary;
+    this.secondaryString = secondary;
+  }
+}
+
+export class Hymn {
+  title: string;
+  number: number;
+  lyrics: Map<String, String>;
+  constructor(title: string, number: number, lyrics: Map<String, String>) {
+    this.title = title;
+    this.number = number;
+    this.lyrics = lyrics;
+  } 
+
+  getSlides(): Array<Slide> {
+    const slides = [new Slide(`${this.number} | ${this.title}`, '')];
+
+    let refrainSlide: Slide | undefined = undefined;
+
+    const stanzas: Array<Slide> = [];
+
+    // All Stanzas are made Slides
+    for (const [context, content] of this.lyrics.entries() as IterableIterator<[string, string]>) {
+      if (Number.isNaN(parseInt(context))) {
+        // Refrain
+        refrainSlide = new Slide(content, context);
+      }
+      else{
+        // Stanza
+        stanzas.push(new Slide(content, context));
+      }
+    }
+    
+    // Put Refrain in front of each stanza
+    if (refrainSlide) {
+      const magicNum = stanzas.length * 2;
+      for (let i = 0; i < magicNum; i++) {
+        if (i % 2 != 0) {
+          stanzas.splice(i, 0, Object.assign({}, refrainSlide));
+        }
+      }
+    }
+    
+    slides.push(...stanzas);
+
+    return slides;
+  }
+}
+
